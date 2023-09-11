2e4b5167bf5886fef16b1f7896e0ebd90beac3d9
Shiva <119954041+TheBrainliestUser@users.noreply.github.com>
2023-09-11 08:01:01 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import random
 import json
-from flask import Flask, jsonify
+from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -9,11 +10,25 @@ def get_quotes():
         quotes = json.load(file)
     return quotes
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def get_random_quote():
     capital = get_quotes()
     random_cap = random.choice(list(capital.keys()))
-    return jsonify({f"What is the capital of {random_cap} ?": f"The capital of {random_cap} is {capital[random_cap]}."})
+    current_datetime = datetime.now()
+    current_date = current_datetime.date()
+    current_time = current_datetime.time()
+
+    return f"""
+    \{
+    Developer : Shiva :),
+    
+    Current Time : {current_time},
+    
+    Current Date : {current_date},
+    
+    What is the capital of {random_cap} ? : The capital of {random_cap} is {capital[random_cap]}.
+    \}
+    """
 
 if __name__ == '__main__':
     app.run(debug=True)
