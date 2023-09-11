52ed85a61b10f12e2a5d280fec552b5d9d77bedd
Shiva <119954041+TheBrainliestUser@users.noreply.github.com>
2023-09-11 08:38:31 +0530

    Add files via upload

--- api.py
+++ api.py
@@ -5,30 +5,20 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-def get_quotes():
+def get_ques():
     with open('quesTest.json', 'r') as file:
-        quotes = json.load(file)
-    return quotes
+        ques = json.load(file)
+    return ques
 
 @app.route('/', methods=['GET'])
-def get_random_quote():
-    capital = get_quotes()
+def get_random_ques():
+    capital = get_ques()
     random_cap = random.choice(list(capital.keys()))
     current_datetime = datetime.now()
     current_date = current_datetime.date()
-    current_time = current_datetime.time()
+    current_time = current_datetime.strftime("%H:%M:%S")
 
-    return f"""
-    \{
-    Developer : Shiva :),
-    
-    Current Time : {current_time},
-    
-    Current Date : {current_date},
-    
-    What is the capital of {random_cap} ? : The capital of {random_cap} is {capital[random_cap]}.
-    \}
-    """
+    return f"Each time you scroll you see a new content here!!<br><hr><br>Developer : Shiva :)<br><br>Current Time : {current_time}<br><br>Current Date : {current_date}<br><br>What is the capital of {random_cap} ? : The capital of {random_cap} is {capital[random_cap]}."
 
 if __name__ == '__main__':
     app.run(debug=True)
